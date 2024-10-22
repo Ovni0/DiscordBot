c2a12280bbdaeaa97ddf436f2e9feb51185f8368
Ovni0 <145059522+Ovni0@users.noreply.github.com>
2024-10-22 22:40:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, GatewayIntentBits, Collection, REST, Routes, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
 const fs = require('fs');
+const config = require('./config.json');
 const { createTicket } = require('./createTicket');
 const { sendWelcomeMessage } = require('./welcome');
 const { sendFarewellMessage } = require('./farewell');
