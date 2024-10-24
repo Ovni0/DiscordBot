5d7893e7ed5a06e238c68178b4ffef1dfaa29f03
Ovni0 <145059522+Ovni0@users.noreply.github.com>
2024-10-24 21:21:39 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -3,17 +3,17 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
     sendWelcomeMessage: (channel, member) => {
         const welcomeEmbed = new EmbedBuilder()
-            .setColor('#ff0000')
-            .setTitle('Bot de Bienvenidas\n👋 Bienvenid@ a La Scoret Army')
+            .setColor('##0097ff')
+            .setTitle('👋 Bienvenid@ a La Scoret Army')
             .setDescription(`Bienvenid@ ${member.user}, pórtate bien.\n
             
-            🚫 Antes que todo, lee el canal ⁠『📕』reglas para evitar cualquier conflicto y estar en paz.
+            🚫 Antes que todo, lee el canal <#1289404786642522195> para evitar cualquier conflicto y estar en paz.
             
-            👥 Si quieres hablar con gente y pasar un buen rato ve a este canal ⁠『💭』general .
+            👥 Si quieres hablar con gente y pasar un buen rato ve a este canal <#1289406027304800266> .
             
-            ❓Si necesitas ayuda en algo, no dudes en consultarla en ⁠『🎫』ticket .
+            ❓Si necesitas ayuda en algo, no dudes en consultarla en <#1289414381356187729>  .
             
-            🗿 No olvides ir a ⁠『📢』redes para seguir a ElScoret en todas sus cuentas.`)
+            🗿 No olvides ir a <#1289414696851472448> para seguir a ElScoret en todas sus cuentas.`)
             .setImage('https://cdn.discordapp.com/attachments/1087323525028777989/1289092940274208809/Bienvenida0.jpg?ex=6717dd9b&is=67168c1b&hm=8836d9d50a111c0917332b1cbc900d31e86b5b7e97e9db3e22598219d99942c5&')
             .setFooter({ text: `❤️ Ahora somos un total de ${channel.guild.memberCount} personas ❤️` });
 
