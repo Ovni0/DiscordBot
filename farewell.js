83a86c438667d867214d3f2a1a4fe3a8cd39dfa8
Ovni0 <145059522+Ovni0@users.noreply.github.com>
2024-10-24 23:17:45 +0200

    Update farewell.js

--- farewell.js
+++ farewell.js
@@ -3,12 +3,15 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
     sendFarewellMessage: (channel, member) => {
         const farewellEmbed = new EmbedBuilder()
-            .setColor('#ff0000')
-            .setTitle('🥀 Cada vez somos menos...')
-            .setDescription(`**\n${member.user} ha abandonado el servidor.**\nNos vemos...\n🥀`)
-            .setImage('https://cdn.discordapp.com/attachments/1087323525028777989/1289094188532498442/Despedida0.jpg?ex=6717dec4&is=67168d44&hm=72dfea80775d1be8b28a30ff5934ba5b167c9e3aa9f068675fc82188e94a90f4&')
-            .setFooter({ text: `💔 Ahora somos un total de ${channel.guild.memberCount} personas 💔` });
+    .setColor('#D32F2F') // Rojo más oscuro para una sensación más solemne
+    .setTitle('🥀 Cada vez somos menos...')
+    .setDescription(`**${member.user.tag}** ha decidido dejarnos.\nEsperamos que nuestros caminos se crucen de nuevo... 💔`)
+    .setImage('https://cdn.discordapp.com/attachments/1087323525028777989/1289094188532498442/Despedida0.jpg?ex=6717dec4&is=67168d44&hm=72dfea80775d1be8b28a30ff5934ba5b167c9e3aa9f068675fc82188e94a90f4&')
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true })) // Añade el avatar del usuario que se va
+    .setFooter({ text: `💔 Ahora somos un total de ${channel.guild.memberCount} personas 💔`, iconURL: 'https://cdn.discordapp.com/icons/YOUR_GUILD_ICON.png' }) // Pie con el total de miembros
+    .setTimestamp(); // Añade la fecha y hora actuales
 
-        channel.send({ embeds: [farewellEmbed] });
+channel.send({ embeds: [farewellEmbed] });
+
     }
 };
